bcd9c1f0de0aab9ec0b58dff2a164967ff788112
Ole Tetlie <ole.tetlie@gmail.com>
2017-04-15 17:30:27 -0400

    Added Peaksensor for continuous timings. Not used anywhere yet.

---
+++ Assets/Game/Scripts/Utils/Peaksensor.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+public class Peaksensor
+{
+    public Peaksensor(string message, int toleranceμs = 100, bool autostart = true)
+    {
+        stopwatch = new Stopwatch();
+        this.message = message;
+        this.toleranceμs = toleranceμs;
+        if (autostart)
+        {
+            Start();
+        }
+    }
+
+    public void Start(bool reset = false)
+    {
+        if (reset)
+        {
+            stopwatch.Reset();
+        }
+
+        stopwatch.Start();
+    }
+
+    public void Stop(bool finished = true)
+    {
+        stopwatch.Stop();
+
+        if (finished)
+        {
+            int elapsedμs = (int)((double)stopwatch.ElapsedTicks / Stopwatch.Frequency * 1000000);
+            if (elapsedμs > toleranceμs)
+            {
+                UnityEngine.Debug.Log(message + " took " + elapsedμs + "μs, expected " + toleranceμs + "μs or less.");
+            }
+        }
+    }
+
+    private Stopwatch stopwatch;
+    private string message;
+    private int toleranceμs;
+}
